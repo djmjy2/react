de8f7d7492fc5352e695a76809a2f6d944e33471
Hwi-Ram <djmjy2@naver.com>
2021-12-20 21:26:56 +0900

    사용자 컴포넌트 활용
    대문자 함수로 만들기

--- src/app.js
+++ src/app.js
@@ -2,8 +2,12 @@
 
 import { createDOM, createElement, render } from './react';
 
+  function Title() {
+    return <h1>React 만들기</h1>;
+  }
+
   const vdom = <p>
-    <h1>React 만들기</h1>
+    <Title></Title>
     <ul>
       <li style="color:red">1번</li>
       <li style="color:blue">2번</li>
