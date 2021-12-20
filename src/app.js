a961b169ad9f6671e44ee583635014a5400b13c1
Hwi-Ram <djmjy2@naver.com>
2021-12-20 21:36:18 +0900

    함수컴포넌트 활용
    컴포넌트를 더 활용해보기

--- src/app.js
+++ src/app.js
@@ -1,17 +1,21 @@
 /* @jsx createElement */
 
-import { createDOM, createElement, render } from './react';
+import { createElement, render } from './react';
 
-  function Title() {
-    return <h1>React 만들기</h1>;
+  function Title(props) {
+    return <h1>{ props.children }</h1>;
+  }
+
+  function Item(props) {
+    return <li style={'color:${props.color}'}>{ props.children }</li>
   }
 
   const vdom = <p>
-    <Title></Title>
+    <Title label="React">React 만들기 !</Title>
     <ul>
-      <li style="color:red">1번</li>
-      <li style="color:blue">2번</li>
-      <li style="color:green">3번</li>
+      <li style="color:red">1번째</li>
+      <li style="color:blue">2번째</li>
+      <li style="color:green">3번째</li>
     </ul>
   </p>
 
