56182cfd1203f2584c15ee6092f58e36bc44d2f1
Hwi-Ram <djmjy2@naver.com>
2021-12-21 21:51:34 +0900

    클래스컴포넌트 사용
    초기화면을 클래스 컴포넌트로

--- src/app.js
+++ src/app.js
@@ -1,22 +1,24 @@
 /* @jsx createElement */
 
-import { createElement, render } from './react';
+import { createElement, render, Component } from './react';
 
-  function Title(props) {
-    return <h1>{ props.children }</h1>;
+class Title extends Component {
+  render() {
+    return <h1>{ this.props.children }</h1>;
   }
+}
 
-  function Item(props) {
+ function Item(props) {
     return <li style={'color:${props.color}'}>{ props.children }</li>
-  }
+}
 
-  const vdom = <p>
-    <Title label="React">React 만들기 !</Title>
-    <ul>
-      <li style="color:red">1번째</li>
-      <li style="color:blue">2번째</li>
-      <li style="color:green">3번째</li>
-    </ul>
-  </p>
+const App = () => <p>
+  <Title label="React">React 클래스컴포넌트 만들기 !</Title>
+  <ul>
+    <li style="color:red">1번째</li>
+    <li style="color:blue">2번째</li>
+    <li style="color:green">3번째</li>
+  </ul>
+</p>
 
-render(vdom, document.querySelector('#root'));
+render(<App />, document.querySelector('#root'));
